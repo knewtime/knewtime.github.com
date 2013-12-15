6dc811f206e59822b551447e02e79d2d20111d99
Lu Jianhua <baboodut@foxmail.com>
2013-12-15 16:27:14 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -22,7 +22,13 @@ $(document).ready(function(){
     };
 
     $('pre').addClass('prettyprint linenums') //添加Google code Hight需要的class
-
+    
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('加载中...');
+        var disqus_shortname = 'beiyuu';
+        var that = this;
+        BYB.includeScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()}); //这是一个加载js的函数
+});
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
